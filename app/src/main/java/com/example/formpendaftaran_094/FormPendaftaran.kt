f362b2acebd527bfcfefde563d2ef770ee4891e8
irjakniibos <irza.yaumil.ft23@mail.umy.ac.id>
2025-10-31 18:02:53 +0700

    Menambahkan spacer kedua

--- app/src/main/java/com/example/formpendaftaran_094/FormPendaftaran.kt
+++ app/src/main/java/com/example/formpendaftaran_094/FormPendaftaran.kt
@@ -95,6 +95,7 @@ fun FormulirPendaftaran(modifier: Modifier = Modifier){
                     Text(item)
                 }
             }
+            Spacer(modifier = Modifier.height(16.dp))
         }
     }
 }
