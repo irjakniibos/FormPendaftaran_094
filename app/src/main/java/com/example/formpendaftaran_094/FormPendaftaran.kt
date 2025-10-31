d76c40616844991307cdf6a43e2f5bd08b102218
irjakniibos <irza.yaumil.ft23@mail.umy.ac.id>
2025-10-31 17:49:28 +0700

    Menambahkan Variabel jenisKelamin

--- app/src/main/java/com/example/formpendaftaran_094/FormPendaftaran.kt
+++ app/src/main/java/com/example/formpendaftaran_094/FormPendaftaran.kt
@@ -1,12 +1,15 @@
 package com.example.formpendaftaran_094
 
 import androidx.compose.runtime.Composable
+import androidx.compose.runtime.getValue
 import androidx.compose.runtime.mutableStateOf
 import androidx.compose.runtime.remember
+import androidx.compose.runtime.setValue
 import androidx.compose.ui.Modifier
 
 @Composable
 fun FormulirPendaftaran(modifier: Modifier = Modifier){
     var nama by remember { mutableStateOf("") }
-
+    var alamat by remember { mutableStateOf("") }
+    var jenisKelamin by remember { mutableStateOf("") }
 }
